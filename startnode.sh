888a4bdf7cb0bae647624dfc0627344e41eeba74
Martin Helfer <martin-helfer@gmx.de>
2018-05-25 23:20:29 +0200

    sleep for button

--- startnode.sh
+++ startnode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/pi/mh_prog/WebSocketAudioMplayerServer
 /usr/bin/sudo /usr/bin/node ./server.js > /home/pi/mh_prog/output-server.txt &
-/bin/sleep 2
+/bin/sleep 5
 cd /home/pi/mh_prog/WebSocketGPIO
 /usr/bin/sudo /usr/bin/node ./button.js > /home/pi/mh_prog/output-button.txt &
