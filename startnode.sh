4960cf167171e2f43d57577e57418c1371f20eaa
Martin Helfer <martin-helfer@gmx.de>
2018-07-03 14:12:24 +0200

    autostart rfid

--- startnode.sh
+++ startnode.sh
@@ -6,3 +6,4 @@ cd /home/pi/mh_prog/WebSocketGPIO
 /usr/bin/sudo /usr/bin/node ./button.js > /home/pi/mh_prog/output-button.txt &
 /usr/bin/sudo /usr/bin/node ./led.js > /home/pi/mh_prog/output-led.txt &
 /usr/bin/sudo /usr/bin/node ./rotary.js > /home/pi/mh_prog/output-rotary.txt &
+/usr/bin/sudo /usr/bin/node ./rfid.js > /home/pi/mh_prog/output-rfid.txt &
