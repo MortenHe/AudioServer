41aabd278d5a35347e700434b01f2b2210468b8c
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2018-10-18 13:06:50 +0200

    config

--- startnode.sh
+++ startnode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/pi/mh_prog/WebSocketAudioMplayerServer
+cd /home/pi/mh_prog/AudioServer
 /usr/bin/sudo /usr/bin/node ./server.js > /home/pi/mh_prog/output-server.txt &
 /bin/sleep 5
 cd /home/pi/mh_prog/WebSocketGPIO
