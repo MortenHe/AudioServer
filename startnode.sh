a098a4fee8c300f24607d9b10f86a8de3fb29e5c
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2019-02-18 14:55:04 +0100

    kein GPIO etc starten

--- startnode.sh
+++ startnode.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /home/pi/mh_prog/AudioServer
 /usr/bin/sudo /usr/bin/node ./server.js > /home/pi/mh_prog/output-server.txt &
-/bin/sleep 5
-cd /home/pi/mh_prog/WebSocketGPIO
-/usr/bin/sudo /usr/bin/node ./button.js > /home/pi/mh_prog/output-button.txt &
-/usr/bin/sudo /usr/bin/node ./led.js > /home/pi/mh_prog/output-led.txt &
-/usr/bin/sudo /usr/bin/node ./rotary.js > /home/pi/mh_prog/output-rotary.txt &
-/usr/bin/sudo /usr/bin/node ./rfid.js > /home/pi/mh_prog/output-rfid.txt &
-/usr/bin/sudo /usr/bin/node ./led-bar.js > /home/pi/mh_prog/output-led-bar.txt &
+#/bin/sleep 5
+#cd /home/pi/mh_prog/WebSocketGPIO
+#/usr/bin/sudo /usr/bin/node ./button.js > /home/pi/mh_prog/output-button.txt &
+#/usr/bin/sudo /usr/bin/node ./led.js > /home/pi/mh_prog/output-led.txt &
+#/usr/bin/sudo /usr/bin/node ./rotary.js > /home/pi/mh_prog/output-rotary.txt &
+#/usr/bin/sudo /usr/bin/node ./rfid.js > /home/pi/mh_prog/output-rfid.txt &
+#/usr/bin/sudo /usr/bin/node ./led-bar.js > /home/pi/mh_prog/output-led-bar.txt &
