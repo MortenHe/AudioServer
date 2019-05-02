0dbd4c01614d6811e5190119f5583a6c2c13484d
Martin Helfer <martin-helfer@gmx.de>
2019-05-02 18:17:37 +0200

    bug fix

--- server.js
+++ server.js
@@ -508,7 +508,7 @@ function sendClientInfo(messageObjArr) {
 
 //Lautstaerke setzen
 function setVolume() {
-    let initialVolumeCommand = "sudo amixer sset " + configFile["audioOutput"] + " " + + data["volume"] + "% -M";
+    let initialVolumeCommand = "sudo amixer sset " + configFile["audioOutput"] + " " + + currentVolume + "% -M";
     console.log(initialVolumeCommand)
     execSync(initialVolumeCommand);
 }
