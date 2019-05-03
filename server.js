07b927b7191ca627694e92c7c19485ec849c164a
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2019-05-03 15:03:26 +0200

    merge

--- server.js
+++ server.js
@@ -425,7 +425,7 @@ function sendClientInfo(messageArr) {
 
 //Lautstaerke setzen
 function setVolume() {
-    let initialVolumeCommand = "sudo amixer sset " + configFile["audioOutput"] + " " + + currentVolume + "% -M";
+    let initialVolumeCommand = "sudo amixer sset " + configFile["audioOutput"] + " " + + data["volume"] + "% -M";
     console.log(initialVolumeCommand)
     execSync(initialVolumeCommand);
 }
