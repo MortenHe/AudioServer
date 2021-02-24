70d87ca57b250543a22a1e210458dd867f78ab15
Martin Helfer <martin-helfer@gmx.de>
2021-02-24 12:02:51 +0100

    SoundQuiz capital

--- startnodesoundplayer.sh
+++ startnodesoundplayer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Server-Script liegt im benachbartem Verzeichnis zum sh-Script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-/usr/bin/sudo /usr/bin/node "${DIR}/../SoundquizServer/server-player.js" > "${DIR}/../output-server.txt" &
+/usr/bin/sudo /usr/bin/node "${DIR}/../SoundQuizServer/server-player.js" > "${DIR}/../output-server.txt" &
