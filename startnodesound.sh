fe8da7cc882e1bfe6025f5333056d38809a21547
Martin Helfer <martin-helfer@gmx.de>
2021-02-24 09:52:33 +0100

    Startskripte für NewSH + Soundquiz in einem Ordner

---
+++ startnodesound.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Server-Script liegt im benachbartem Verzeichnis zum sh-Script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+/usr/bin/sudo /usr/bin/node "${DIR}/../SoundquizServer/server.js" > "${DIR}/../output-server.txt" &
