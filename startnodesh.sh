959c20e7a99baf1edf6d79f16778578e4ef06d0d
Martin Helfer <martin-helfer@gmx.de>
2021-03-01 09:27:57 +0100

    Startnodesh / Quiz wieder mit Parameter

--- startnodesh.sh
+++ startnodesh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Server-Script liegt im benachbartem Verzeichnis zum sh-Script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-/usr/bin/sudo /usr/bin/node "${DIR}/../NewSHAudioServer/server.js" > "${DIR}/../output-server.txt" &
+/usr/bin/sudo /usr/bin/node "${DIR}/../NewSHAudioServer/server.js" ${1:-''} > "${DIR}/../output-server.txt" &
--- startnodesound.sh
+++ startnodesound.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Server-Script liegt im benachbartem Verzeichnis zum sh-Script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-/usr/bin/sudo /usr/bin/node "${DIR}/../SoundQuizServer/server.js" > "${DIR}/../output-server.txt" &
+/usr/bin/sudo /usr/bin/node "${DIR}/../SoundQuizServer/server.js" ${1:-''} > "${DIR}/../output-server.txt" &
